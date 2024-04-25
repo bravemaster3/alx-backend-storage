86796064335e60cdbb2ef2482297aef371c04448
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-25 22:05:24 +0000

    Add 101

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+BEGIN
+    -- Calculate the weighted sum and sum of weights for all users
+    UPDATE users u
+    JOIN (
+        SELECT c.user_id,
+               SUM(c.score * p.weight) AS weighted_sum_score,
+               SUM(p.weight) AS sum_weights
+        FROM corrections c
+        INNER JOIN projects p ON c.project_id = p.id
+        GROUP BY c.user_id
+    ) AS t ON u.id = t.user_id
+    SET u.average_score = IF(t.sum_weights > 0, t.weighted_sum_score / t.sum_weights, 0);
+END//
+
+DELIMITER ;
